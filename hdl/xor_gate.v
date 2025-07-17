5aeaee729de17afa16c8b6e36b63df9e6a27cf86
Kartikaye <119552388+Napier1192@users.noreply.github.com>
2025-07-17 16:39:10 +0530

    Update xor_gate.v

--- hdl/xor_gate.v
+++ hdl/xor_gate.v
@@ -1,31 +1,6 @@
-`timescale 1ns/1ps
-
-module xor_gate(input wire a, input wire b, output wire y);
-  assign y = a ^ b;
-endmodule
-
-module xor_tb;
-  reg a, b;
-  wire y;
-  xor_gate uut (
-    .a(a),
-    .b(b),
-    .y(y)
-  );
-
-  initial begin
-    $dumpfile("xor.vcd");
-    $dumpvars(0, xor_tb);
-
-    a = 0; b = 0;
-    #10;
-    a = 0; b = 1;
-    #10;
-    a = 1; b = 0;
-    #10;
-    a = 1; b = 1;
-    #10;
-    
-    $finish;
-  end
+module xor_gate(input wire a,
+	input wire b,
+	output wire y
+);
+assign y=a^b;
 endmodule
